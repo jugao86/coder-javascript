0ab9ea4eb53efb217c933d69e15b9213dba3926f
Julio Gallaga <juliogallaga@MacBook-Air-de-Julio.local>
2022-08-30 14:35:54 -0500

    Se mejoro el ejercicio y se tomaron en cuenta los comentarios

---
+++ js/scriptEntregaUno.js
@@ -0,0 +1,18 @@
+//Entrega uno
+
+var nombre = prompt("Ingresa tu nombre");
+var edad = Number(prompt("Ahora tu edad"));
+
+let resultado = "Hola" + " " + nombre + " " + "Tienes hasta" + " " + edad + " " + "para esconderte";
+
+
+alert(resultado);
+
+let x = 0;
+
+do {
+  console.log(`${x}`);
+  x++;
+} while (x <= edad);
+
+alert("¡Un" + " " + "Dos" + " "  + "Tres!" + " " + "por" + " " + nombre + " " + "que esta detras del monitor");
